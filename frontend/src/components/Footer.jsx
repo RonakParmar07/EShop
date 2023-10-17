64c8afe5ed6d592ac6c4686df2dc6001a527906f
Ronak <rjparmar014@gmail.com>
2023-10-17 13:11:28 +0530

    Header, Footer, Rating and Product Components and Homescreen and Product detail scrren all this with React Router

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <Row>
+        <Col className='text-center py-3'>
+          <p>EShop &copy; {currentYear}</p>
+        </Col>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
