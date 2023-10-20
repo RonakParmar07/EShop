cf1b6c289b94c075e6902fcd049755a176c064d2
Ronak <rjparmar014@gmail.com>
2023-10-20 22:34:54 +0530

    Add to cart functionality, cartSlice, add and remove from cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/cartScreen";
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
+
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
